5e0fe18d0978347fa5bca5de74b66df536f848c5
Luke Pura <lukepura@gmail.com>
2018-03-16 17:01:59 -0500

    AR work

--- components/AR-Clue.js
+++ components/AR-Clue.js
@@ -1,5 +1,6 @@
 import Expo from 'expo';
 import React from 'react';
+import { TouchableOpacity } from 'react-native'
 
 import * as THREE from 'three'; // 0.87.1
 import ExpoTHREE from 'expo-three'; // 2.0.2
@@ -8,17 +9,15 @@ console.disableYellowBox = true;
 
 export default class Clue extends React.Component {
 
-    constructor() {
-        super()
-    }
-
     render() {
         return (
+
             <Expo.GLView
                 ref={(ref) => this._glView = ref}
                 style={{ flex: 1 }}
                 onContextCreate={this._onGLContextCreate}
             />
+
         );
     }
 
@@ -37,35 +36,48 @@ export default class Clue extends React.Component {
 
         // Edit the box dimensions here and see changes immediately!
 
-        const textureLoader = new THREE.TextureLoader();
+        const makeCube = () => {
+            const textureLoader = new THREE.TextureLoader();
 
-        const texture0 = textureLoader.load('./assets/img/clue_side1.png');
-        const texture1 = textureLoader.load('./assets/img/clue_side2.png');
-        const texture2 = textureLoader.load('./assets/img/clue_side3.png');
-        const texture3 = textureLoader.load('./assets/img/clue_side4.png');
-        const texture4 = textureLoader.load('./assets/img/clue_side5.png');
-        const texture5 = textureLoader.load('./assets/img/clue_side6.png');
+            const texture0 = textureLoader.load('./assets/img/clue_side1.png');
+            const texture1 = textureLoader.load('./assets/img/clue_side2.png');
+            const texture2 = textureLoader.load('./assets/img/clue_side3.png');
+            const texture3 = textureLoader.load('./assets/img/clue_side4.png');
+            const texture4 = textureLoader.load('./assets/img/clue_side5.png');
+            const texture5 = textureLoader.load('./assets/img/clue_side6.png');
 
-        const texture = new THREE.TextureLoader().load('./assets/img/clue_side1.png')
+            // let texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/crate.gif')
 
+            var materials = [
+                new THREE.MeshBasicMaterial({ map: texture0 }),
+                new THREE.MeshBasicMaterial({ map: texture1 }),
+                new THREE.MeshBasicMaterial({ map: texture2 }),
+                new THREE.MeshBasicMaterial({ map: texture3 }),
+                new THREE.MeshBasicMaterial({ map: texture4 }),
+                new THREE.MeshBasicMaterial({ map: texture5 })
+            ];
 
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ map: texture });
-        const cube = new THREE.Mesh(geometry, material);
-        cube.position.z = -5;
-        cube.position.y = .5;
-        scene.add(cube);
+            const geometry = new THREE.BoxGeometry(1, 1, 1);
+            const cube = new THREE.Mesh(geometry, materials);
 
-        const animate = () => {
-            requestAnimationFrame(animate);
+            cube.position.z = -5;
+            cube.position.y = 0.5;
 
-            cube.rotation.x += 0.07;
-            cube.rotation.y += 0.04;
+            scene.add(cube);
 
-            renderer.render(scene, camera);
-            gl.endFrameEXP();
+            const animate = () => {
+                requestAnimationFrame(animate);
+
+                cube.rotation.x += 0.07;
+                cube.rotation.y += 0.04;
+
+                renderer.render(scene, camera);
+                gl.endFrameEXP();
+            }
+            animate();
         }
-        animate();
+        makeCube();
     }
+
 }
 
