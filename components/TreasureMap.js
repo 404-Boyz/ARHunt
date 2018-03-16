eb73edb3bae9c52997fc9e56618550fd0f54c2e7
Tom Sziler <tom.sziler@gmail.com>
2018-03-16 11:59:29 -0500

    some progress made on map

--- components/TreasureMap.js
+++ components/TreasureMap.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 import { StackNavigator, navigationOptions } from 'react-navigation';
-import { MapView, Marker } from 'expo';
+import { MapView, Location } from 'expo';
 
 export default class TreasureMap extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      position: {
+        latitude: 0,
+        longitude: 0
+      },
+      locationFinder: {}
+    }
   }
+
+  
+  componentDidMount() {
+   const locationFinder = Location.watchPositionAsync({enableHighAccuracy:true, distanceInterval: 1}, (position)=> {this.setState({position: {latitude: position.coords.latitude, longitude: position.coords.longitude}}); console.log(this.state.position)})
+   this.setState({locationFinder})
+  }
+
+  componentWillUnmount() {
+  this.state.locationFinder.remove();
+  }
+
   render() {
+    console.log(this.state.locationFinder);
     return (
       <MapView
         style={{ flex: 1 }}
+       showsUserLocation={true}
         initialRegion={{
           latitude: 41.895266,
           longitude: -87.639035,
@@ -18,6 +38,11 @@ export default class TreasureMap extends Component {
           longitudeDelta: 0.0421,
         }}
       >
+      <MapView.Marker
+        coordinate={testMarker.coordinate}
+        title="FullStack"
+        description="Our school"
+      />
       </MapView>
     );
   }
