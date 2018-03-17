cac0f40dec154d274126672bda6e4e2aa98c5828
Wayne Temple <waynetemple@gmail.com>
2018-03-17 13:24:03 -0500

    building database and auth

--- client/components/Navigator.js
+++ client/components/Navigator.js
@@ -33,7 +33,7 @@ const RootStack = StackNavigator({
 
 },
   {
-    initialRouteName: 'Home'
+    initialRouteName: 'Login'
   }
 );
 
---
+++ server/db/models/index.js
@@ -0,0 +1,12 @@
+const User = require('./user');
+const Location = require('./location');
+const Adventure = require('./adventure');
+
+
+
+
+module.exports = {
+  User,
+  Location,
+  Adventure
+}
