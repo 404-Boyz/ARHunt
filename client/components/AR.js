df74e43dff6c8b092e7163038910ef3d1ffebf84
Luke Pura <lukepura@gmail.com>
2018-03-19 15:50:03 -0500

    Cube laoding on location

--- client/components/AR.js
+++ client/components/AR.js
@@ -1,10 +1,12 @@
 
-import Expo from 'expo';
+import Expo, { Location } from 'expo';
 import React, { Component } from 'react';
-import { TouchableOpacity, View, Modal, TouchableHighlight, Text, Dimensions } from 'react-native'
+import { TouchableOpacity, View, Modal, TouchableHighlight, Text, Dimensions, Vibration, TouchableWithoutFeedback } from 'react-native'
 
 import * as THREE from 'three'; // 0.87.1
 import ExpoTHREE from 'expo-three'; // 2.0.2
+
+import geolib from 'geolib';
 
 console.disableYellowBox = true;
 
@@ -14,11 +16,47 @@ export default class AR extends React.Component {
         super(props);
         this.state = {
             modalVisible: false,
+            currentPosition: {
+                latitude: 0,
+                longitude: 0
+            },
+            nextPosition: {
+                latitude: 41.895169,
+                longitude: -87.639029,
+            },
+            distToNext: NaN,
+            isInside: false,
+            counter: 0
         };
+        this._onGLContextCreate = this._onGLContextCreate.bind(this);
+        this.makeCube = this.makeCube.bind(this);
     }
 
     touch = new THREE.Vector2();
     raycaster = new THREE.Raycaster();
+
+    componentDidMount() {
+        this.locationFinder();
+    }
+
+    // checking current location and distance to clue
+
+    locationFinder = async () => {
+        await Location.watchPositionAsync({ enableHighAccuracy: true, distanceInterval: 1 },
+            (position) => {
+
+                this.setState({ currentPosition: { latitude: position.coords.latitude, longitude: position.coords.longitude } });
+                this.setState({ distToNext: geolib.getDistance(this.state.currentPosition, this.state.nextPosition, 1) });
+                this.setState({ isInside: geolib.isPointInCircle(this.state.currentPosition, this.state.nextPosition, 20) }, () => {
+                    if (this.state.isInside && this.state.counter === 0) {
+                        this.state.counter++
+                        this.makeCube(this.gl)
+                    }
+                });
+            })
+    }
+
+    // grab the touch location and match it to the screen
 
     updateTouch = (evt) => {
         let { height, width } = Dimensions.get('window')
@@ -26,20 +64,25 @@ export default class AR extends React.Component {
         let y = evt.nativeEvent.locationY
         this.touch.x = x / width * 2 - 1;
         this.touch.y = -(y / height) * 2 + 1;
-        console.log('x', x)
-        console.log('HIT 1')
         this.runHitTest();
     };
+
+    // check the touch location against the raycaster and see if it matches our cube
 
     runHitTest = () => {
         this.raycaster.setFromCamera(this.touch, this.camera);
         const intersects = this.raycaster.intersectObjects(this.scene.children);
         if (intersects.length > 0) {
             this.setModalVisible(!this.state.modalVisible)
+        } else {
+            Vibration.vibrate()
         }
     };
 
+    // set up the AR scene with scene, camera and render
+
     _onGLContextCreate = async (gl) => {
+        this.gl = gl;
         this.glWidth = gl.drawingBufferWidth;
         this.glHeight = gl.drawingBufferHeight;
         this.arSession = await this._glView.startARSessionAsync();
@@ -50,32 +93,56 @@ export default class AR extends React.Component {
         this.renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
         this.scene.background = ExpoTHREE.createARBackgroundTexture(this.arSession, this.renderer);
 
-        const geometry = new THREE.BoxGeometry(1.2, 1.2, 1.2);
+        this.makeCube(this.gl);
+    }
 
-        for (let i = 0; i < geometry.faces.length; i += 2) {
-            let hex = Math.random() * 0xffffff;
-            geometry.faces[i].color.setHex(hex);
-            geometry.faces[i + 1].color.setHex(hex);
+    // create the AR cube
+
+    makeCube(gl) {
+        let animate;
+        console.log('IN', this.state.distToNext)
+
+        // checking distance to clue location and rendering cube or not based on that
+
+        if (this.state.distToNext < 20) {
+            const geometry = new THREE.BoxGeometry(1.4, 1.4, 1.4);
+
+            // randomizing the cube colors and creating the 3D/AR shape
+
+            for (let i = 0; i < geometry.faces.length; i += 2) {
+                let hex = Math.random() * 0xffffff;
+                geometry.faces[i].color.setHex(hex);
+                geometry.faces[i + 1].color.setHex(hex);
+            }
+            const material = new THREE.MeshBasicMaterial({ vertexColors: THREE.FaceColors, overdraw: 0.5 });
+            const cube = new THREE.Mesh(geometry, material);
+
+            cube.position.z = -8;
+            cube.position.y = 0.8;
+
+            this.scene.add(cube);
+
+            // run the AR scene & camera with the cube
+
+            animate = () => {
+                requestAnimationFrame(animate);
+
+                cube.rotation.x += 0.07;
+                cube.rotation.y += 0.04;
+
+                this.renderer.render(this.scene, this.camera);
+                gl.endFrameEXP();
+            }
+        } else {
+
+            // run the AR scene & camera without the cube
+
+            animate = () => {
+                requestAnimationFrame(animate);
+                this.renderer.render(this.scene, this.camera);
+                gl.endFrameEXP();
+            }
         }
-        const material = new THREE.MeshBasicMaterial({ vertexColors: THREE.FaceColors, overdraw: 0.5 });
-        const cube = new THREE.Mesh(geometry, material);
-
-        cube.position.z = -10;
-        cube.position.y = 0.8;
-
-        this.scene.add(cube);
-
-
-        const animate = () => {
-            requestAnimationFrame(animate);
-
-            cube.rotation.x += 0.07;
-            cube.rotation.y += 0.04;
-
-            this.renderer.render(this.scene, this.camera);
-            gl.endFrameEXP();
-        }
-
         animate();
     }
 
