1c42b283f7c131581753ba0ee776d650dce4bea1
Wayne Temple <waynetemple@gmail.com>
2018-03-19 11:23:33 -0500

    seed file created --- modified models

--- server/db/models/location.js
+++ server/db/models/location.js
@@ -3,6 +3,8 @@ const db = require('../db');
 
 const Location = db.define('location', {
   name: Sequelize.STRING,
+  clue: Sequelize.STRING,
+  description: Sequelize.STRING,
   latitude: Sequelize.DECIMAL,
   longitude: Sequelize.DECIMAL,
   positionInHunt: Sequelize.INTEGER
