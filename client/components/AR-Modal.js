947ebf2b215a8626acb7bf4a1afb5589478e08e0
Eric Roth <sirviver@gmail.com>
2018-03-21 15:40:01 -0500

    some modal work

--- client/components/AR-Modal.js
+++ client/components/AR-Modal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Modal, View, Text, TouchableHighlight } from 'react-native';
+import { Button, Container, Header, Content, Card, CardItem, Body } from 'native-base'
 
 export default class ARModal extends Component {
   constructor(props) {
@@ -7,25 +8,44 @@ export default class ARModal extends Component {
   }
   render(props) {
     return (
-      <Modal
-        animationType="slide"
-        transparent={false}
-        onRequestClose={() => {
-          alert('Modal has been closed.');
-        }}>
-        <View style={{ marginTop: 22 }}>
-          <View>
-            <Text>Hello World!</Text>
-
-            <TouchableHighlight
-              onPress={() => {
-                this.props.setModalVisible(false);
-              }}>
-              <Text>Hide Modal</Text>
-            </TouchableHighlight>
-          </View>
-        </View>
-      </Modal>
+      <Container>
+        <Header />
+        <Modal
+          animationType="slide"
+          transparent={false}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+          }}>
+          <Content>
+            <Card>
+              <CardItem header>
+                <Text>Clue Title</Text>
+              </CardItem>
+              <View style={{ marginTop: 22 }}>
+              <View>
+                <CardItem>
+                  <Body>
+                    <Text>
+                      Clue Text
+                    </Text>
+                  </Body>
+                  </CardItem>
+                  <CardItem footer>
+                  <Button rounded>
+                    <TouchableHighlight
+                      onPress={() => {
+                        this.props.setModalVisible(false);
+                      }}>
+                      <Text>  Close Clue  </Text>
+                    </TouchableHighlight>
+                  </Button>
+                  </CardItem>
+                  </View>
+                </View>
+            </Card>
+          </Content>
+        </Modal>
+      </Container>
     )
   }
 }
